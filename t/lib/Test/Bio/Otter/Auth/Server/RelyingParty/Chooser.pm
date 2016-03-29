c85e6b630b7b10830cb5d7fa26f1177d5c131eea
Michael Gray <mg13@sanger.ac.uk>
2016-03-29 17:29:44 +0100

    Test:B:O:Auth::Server::RelyingParty*

---
+++ t/lib/Test/Bio/Otter/Auth/Server/RelyingParty/Chooser.pm
@@ -0,0 +1,25 @@
+package Test::Bio::Otter::Auth::Server::RelyingParty::Chooser;
+
+use Test::Class::Most
+    parent     => 'Test::Bio::Otter::Auth::Server';
+
+# This smells a bit:
+#
+use HTTP::Request;
+use HTTP::Message::PSGI;        # inject to_psgi()
+use Plack::Request;
+use Plack::Response;
+
+sub build_attributes { return; } # none
+
+sub our_args {
+    my $req = HTTP::Request->new(GET => '/');
+    my $env = $req->to_psgi;
+    return [
+        request  => Plack::Request->new($env),
+        response => Plack::Response->new(),
+        ];
+}
+
+1;
+
