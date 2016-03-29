e9209b35fc7623d93d189d3c7059ead8356b0e05
Michael Gray <mg13@sanger.ac.uk>
2016-03-29 17:22:58 +0100

    Test:B:O:Auth::Server::OIDCProvider*

---
+++ t/lib/Test/Bio/Otter/Auth/Server/OIDCProvider.pm
@@ -0,0 +1,11 @@
+package Test::Bio::Otter::Auth::Server::OIDCProvider;
+
+use Test::Class::Most
+    parent     => 'OtterTest::Class';
+
+sub build_attributes { return; } # none
+
+sub setup       { return; }  # don't let OtterTest::Class do its OO stuff
+sub constructor { return; }  # --"--
+
+1;
